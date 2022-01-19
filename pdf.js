42c4988a9cb62c1a6c83b1e6befdccb7a28a0788
Anish M Prasad <anish.m.prasad@gmail.com>
2022-01-19 15:05:45 +0530

    cleanup

--- pdf.js
+++ pdf.js
@@ -1,12 +1,18 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://news.ycombinator.com', {
-    waitUntil: 'networkidle2',
-  });
-  await page.pdf({ path: 'anishmprasad.pdf', format: 'a4' });
+    const url = 'https://youtube.com'
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, {
+        waitUntil: 'networkidle2',
+    });
+    await page.pdf({ path: `${url}.pdf`, format: 'a4' });
 
-  await browser.close();
+    await browser.close();
 })();
+
+
+
+// https://github.com/puppeteer/puppeteer/blob/v13.1.1/docs/api.md#pagepdfoptions
+// https://news.ycombinator.com
