5ca98dec62c1e99eb98ace798813e836339746a5
Anish M Prasad <anish.m.prasad@gmail.com>
2022-01-19 13:47:05 +0530

    cleanup

--- pdf.js
+++ pdf.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
   await page.goto('https://news.ycombinator.com', {
     waitUntil: 'networkidle2',
   });
-  await page.pdf({ path: 'hn.pdf', format: 'a4' });
+  await page.pdf({ path: 'anishmprasad.pdf', format: 'a4' });
 
   await browser.close();
 })();
