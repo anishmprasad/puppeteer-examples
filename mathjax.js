8a9f6bd711dee223b9057cec6c83a81135ce06a7
Anish Prasad <anish.m.prasad@gmail.com>
2022-04-11 23:10:19 +0530

    code-cleanup

--- mathjax.js
+++ mathjax.js
@@ -1,26 +1,41 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const url = 'https://www.sqlpac.com/en/documents/html-equations-math-with-mathjax-asciimath.html'
+    // const url = 'https://www.sqlpac.com/en/documents/html-equations-math-with-mathjax-asciimath.html'
+    const url = '/template/mathjax.html'
+    
     // const url = 'https://news.google.com'
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        // headless : true,
+        devtools: true
+    });
     const page = await browser.newPage();
-    await page.goto(url, {
+    await page.goto(`file://${__dirname}${url}`,
+    {
         waitUntil: 'networkidle2', //networkidle2
         // waitUntil: 'load',
         // Remove the timeout
         timeout: 0
     });
-    
-    // await page.waitForSelector('.MathJax', {
-    //     visible: true,
-    //   });
+
+    // await page.goto(url, {
+    //     waitUntil: 'networkidle2', //networkidle2
+    //     // waitUntil: 'load',
+    //     // Remove the timeout
+    //     timeout: 0
+    // });
+
+    await page.waitForSelector('.domReasdy', {
+        visible: true,
+    });
+
+    await page.waitFor(5000);
 
     // await page.waitForNavigation({
     //     waitUntil: 'networkidle0',
     //   });
 
-    await page.pdf({ path: `mathjax1.pdf`, format: 'a4' });
+    // await page.pdf({ path: `mathjax${new Date()}.pdf`, format: 'a4' });
 
     // await page.pdf({
     //     path: outputFileName,
